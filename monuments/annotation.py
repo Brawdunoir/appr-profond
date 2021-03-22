a500d9c66ddbfd55d1c03ce43360519872269ba8
Cyril Figuin <65870061+cyrilfgn974@users.noreply.github.com>
2021-03-22 20:58:26 -0100

    Update annotations.py.

--- monuments/annotation.py
+++ monuments/annotation.py
@@ -15,3 +15,5 @@ directory = args["directory"]
 name = args["name"]
 [os.rename(directory+f, str(name) + str(f)) for f in os.listdir(directory) 
 if (f.endswith(".jpg"))]
+
+### a revoir : les fichiers sont bien renommés mais sont déplacés dans le dossier où se situe annotation.py
